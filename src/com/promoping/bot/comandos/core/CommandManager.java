8c9aa75a20cbc544c51280b9b8f9bd84f07e7440
juliareboucasleite <69313019+juliareboucasleite@users.noreply.github.com>
2026-01-28 19:28:34 +0000

    Update CommandManager.java

--- src/com/promoping/bot/comandos/core/CommandManager.java
+++ src/com/promoping/bot/comandos/core/CommandManager.java
@@ -4,13 +4,12 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class CommandManager {
 
     private static final Map<String, Command> COMMANDS = new HashMap<>();
-
     public static void register(Command command) {
         COMMANDS.put(command.getName(), command);
-
         for (String alias : command.getAliases()) {
             COMMANDS.put(alias, command);
         }
@@ -20,7 +19,6 @@ public class CommandManager {
     public static Command get(String name) {
         return COMMANDS.get(name);
     }
-
     public static Collection<Command> all() {
         return COMMANDS.values();
     }
