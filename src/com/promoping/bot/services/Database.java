1f10e31cadd0a94186448c6c4c4207b2c5c0a840
juliareboucasleite <69313019+juliareboucasleite@users.noreply.github.com>
2026-01-28 19:29:40 +0000

    Update Database.java

--- src/com/promoping/bot/services/Database.java
+++ src/com/promoping/bot/services/Database.java
@@ -1,5 +1,4 @@
 package com.promoping.bot.services;
-
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -19,18 +18,14 @@ public class Database {
             try (InputStream in = new FileInputStream("config.properties")) {
                 props.load(in);
             }
-
             url = props.getProperty("DB_URL");
             user = props.getProperty("DB_USER");
             password = props.getProperty("DB_PASSWORD");
-
             Class.forName("com.mysql.cj.jdbc.Driver");
-
         } catch (Exception e) {
             throw new RuntimeException("Erro ao iniciar a base de dados", e);
         }
     }
-
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(url, user, password);
     }
