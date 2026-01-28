700ff36467d57f94dcc4aad6ea3f2a334016c2b2
juliareboucasleite <69313019+juliareboucasleite@users.noreply.github.com>
2026-01-28 19:29:17 +0000

    Update ButtonListener.java

--- src/com/promoping/bot/listeners/ButtonListener.java
+++ src/com/promoping/bot/listeners/ButtonListener.java
@@ -1,4 +1,35 @@
 package com.promoping.bot.listeners;
 
-public class ButtonListener {
+import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.EmbedBuilder;
+
+public class ButtonListener extends ListenerAdapter {
+
+    @Override
+    public void onButtonInteraction(ButtonInteractionEvent event) {
+
+        switch (event.getComponentId()) {
+
+            case "abrir_formulario_sugestao":
+                event.replyEmbeds(
+                        new EmbedBuilder()
+                                .setTitle("Sugestão")
+                                .setDescription("📋 Formulário de sugestão será aberto aqui.")
+                                .setColor(0x3B82F6)
+                                .build()
+                ).setEphemeral(true).queue();
+                break;
+
+            case "abrir_formulario_bug":
+                event.reply("🐛 Formulário de bug em breve.")
+                        .setEphemeral(true).queue();
+                break;
+
+            case "abrir_ticket_promoping":
+                event.reply("🎫 Criando ticket...")
+                        .setEphemeral(true).queue();
+                break;
+        }
+    }
 }
