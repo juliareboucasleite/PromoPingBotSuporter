b6d57b598a2be896950f8ef2d2106981fa288171
juliareboucasleite <69313019+juliareboucasleite@users.noreply.github.com>
2026-01-28 17:06:03 +0000

    Update Command interface with aliases and adminOnly
    Added getAliases and adminOnly methods to the Command interface, and modified the execute method to accept arguments. These changes support command aliasing, admin-only commands, and improved argument handling.

--- src/com/promoping/bot/comandos/Command.java
+++ src/com/promoping/bot/comandos/Command.java
@@ -3,9 +3,12 @@ package com.promoping.bot.comandos;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public interface Command {
+
     String getName();
+    String[] getAliases();
     String getDescription();
     String getCategory();
+    boolean adminOnly();
 
-    void execute(MessageReceivedEvent event);
+    void execute(MessageReceivedEvent event, String[] args);
 }
