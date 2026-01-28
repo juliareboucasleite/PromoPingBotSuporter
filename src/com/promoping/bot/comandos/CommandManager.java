0d023a8307e5d2401ee9048eef7662decaf31ef7
juliareboucasleite <69313019+juliareboucasleite@users.noreply.github.com>
2026-01-28 15:54:44 +0000

    Create CommandManager.java

---
+++ src/com/promoping/bot/comandos/CommandManager.java
@@ -0,0 +1,22 @@
+package com.promoping.bot.comandos;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandManager {
+
+    private static final Map<String, Command> COMMANDS = new HashMap<>();
+
+    public static void register(Command command) {
+        COMMANDS.put(command.getName(), command);
+    }
+
+    public static Command get(String name) {
+        return COMMANDS.get(name);
+    }
+
+    public static Collection<Command> all() {
+        return COMMANDS.values();
+    }
+}
