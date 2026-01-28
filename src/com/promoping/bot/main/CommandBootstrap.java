972cf8ab55e2c47f091778d270005d76bf7ebe46
juliareboucasleite <69313019+juliareboucasleite@users.noreply.github.com>
2026-01-28 19:33:10 +0000

    Update CommandBootstrap.java

--- src/com/promoping/bot/main/CommandBootstrap.java
+++ src/com/promoping/bot/main/CommandBootstrap.java
@@ -14,24 +14,34 @@ public class CommandBootstrap {
     public static void registerAll() {
 
         AccessControl accessControl = new AccessControl();
+
         Database database = new Database();
+
         ReviewDAO reviewDAO = new ReviewDAO(database);
         CountingDAO countingDAO = new CountingDAO(database);
+
         TicketService ticketService = new TicketService(database);
         ChannelService channelService = new ChannelService();
         StatusService statusService = new StatusService();
+
         CommandManager.register(new Help());
         CommandManager.register(new StatusCommand(statusService));
+
         CommandManager.register(new ReviewCommand(reviewDAO));
         CommandManager.register(new SuporteCommand(ticketService));
+
         CommandManager.register(new RulesCommand());
         CommandManager.register(new ReportarCommand());
         CommandManager.register(new SugerirCommand());
+
         CommandManager.register(new CountingCommand(countingDAO, accessControl));
+
         CommandManager.register(new LockCommand(channelService, accessControl));
         CommandManager.register(new UnlockCommand(channelService, accessControl));
+
         CommandManager.register(new SetupTicketCommand(ticketService, accessControl));
         CommandManager.register(new FecharTicketCommand(ticketService, accessControl));
+
         CommandManager.register(new SetupBugCommand(accessControl));
         CommandManager.register(new SetupSugestaoCommand(accessControl));
         CommandManager.register(new InvitePanelCommand(accessControl));
@@ -40,4 +50,5 @@ public class CommandBootstrap {
         System.out.println("Comandos registados");
     }
 }
+
 
